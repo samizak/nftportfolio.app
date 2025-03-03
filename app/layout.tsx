7fb16b9bb6c8d438500beee3dddee5499d5b9c98
Sami Zakir Ahmed <54m1z4k@gmail.com>
2025-03-03 14:34:42 +0000

    renamed project title

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "NFT Portfolio Tracker",
+  title: "nftportfolio.app",
   description: "Track your NFT portfolio value and collections",
 };
 
