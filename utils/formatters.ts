753384e31e73d7297f9c3a1cdf0eebfe8a4b2293
Sami Zakir Ahmed <54m1z4k@gmail.com>
2025-03-04 16:53:17 +0000

    improved formatThousandSeparator to include decimal places

--- utils/formatters.ts
+++ utils/formatters.ts
@@ -1,8 +1,22 @@
 /**
  * Format a number with thousand separators
  */
-export const formatThousandSeparator = (num: number): string => {
-  return new Intl.NumberFormat("en-US").format(num);
+export const formatThousandSeparator = (
+  num: number,
+  fractionDigits?: number
+): string => {
+  // Determine fraction count:
+  // If fractionDigits is provided, use that.
+  // Otherwise, count decimals in the number's string representation.
+  const parts = num.toString().split(".");
+  const detectedFractionDigits = parts[1]?.length || 0;
+  const fractionCount =
+    fractionDigits !== undefined ? fractionDigits : detectedFractionDigits;
+
+  return new Intl.NumberFormat("en-US", {
+    minimumFractionDigits: fractionCount,
+    maximumFractionDigits: fractionCount,
+  }).format(num);
 };
 
 /**
