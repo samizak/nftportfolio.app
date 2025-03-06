635b30a51be3b0c6ca8131fc858bbd7ec5100217
Sami Zakir Ahmed <54m1z4k@gmail.com>
2025-03-06 13:19:15 +0000

    allowed all domains in nxet.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.seadn.io", "openseauserdata.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
